7ce183040a33335f676819604fcd6caecd62c16a
E I <154758787+0xItoHub@users.noreply.github.com>
2024-08-28 14:13:35 +0900

    Add files via upload

---
+++ src/used/SubCalc.java
@@ -0,0 +1,13 @@
+package used;
+
+/**
+ * Calculator実装クラス<br>
+ * 減算を行う
+ */
+
+public class SubCalc implements Calculator {
+	@Override
+	public Integer calc(Integer x, Integer y){
+		return x - y;
+	} 
+}
